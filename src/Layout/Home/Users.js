f07377bcd487eb075ddf15e0c25f95d9f1c41866
Massii <massinechiche@gmail.com>
2022-02-15 18:22:36 +0100

    style: user cards tweaks

--- src/Layout/Home/Users.js
+++ src/Layout/Home/Users.js
@@ -1,47 +1,50 @@
-import React from 'react'
-import { FaGithub, FaLinkedin, FaGlobe } from 'react-icons/fa'
+import React from "react";
+import { FaGithub, FaLinkedin, FaGlobe } from "react-icons/fa";
 
 export default function Users() {
   return (
-    <div className='w-48 h-fit mt-8 shadow-lg p-4'>
-      <div className='w-full flex justify-center items-center'>
+    <div
+      className="w-48 h-fit ml-8 mt-8  border-gray-900 px-8 py-6"
+      style={{ border: "1px solid #f0f0f0 " }}
+    >
+      <div className="w-full flex justify-center items-center">
         <div>
           <img
-            src='https://api.uifaces.co/our-content/donated/IPh6PTBx.jpg'
-            alt=''
-            className='rounded-full w-full'
+            src="https://api.uifaces.co/our-content/donated/IPh6PTBx.jpg"
+            alt=""
+            className="rounded-full p-4"
           />
-          <div className='text-center '>John Doe</div>
+          <div className="text-center ">John Doe</div>
         </div>
       </div>
-      <div className='w-full mt-2 text-gray-800'>
-        <div className='flex items-center gap-2'>
-          <div className='p-1 shadow-inner bg-gray-400/[.15] rounded-full'>
-            <FaGithub className=' text-lg' />
+      <div className="w-full text-gray-800 mt-2">
+        <div className="flex items-center gap-4">
+          <div className="p-1 shadow-inner  rounded-full">
+            <FaGithub className=" text-2xl" />
           </div>
 
-          <div>
-            <a href='!#'>Github</a>
+          <div className="ml-3">
+            <a href="!#">Github</a>
           </div>
         </div>
-        <div className='flex items-center gap-2 mt-1'>
-          <div className='p-1 shadow-inner bg-gray-400/[.15] rounded-full'>
-            <FaLinkedin className='text-lg' />
+        <div className="flex items-center gap-4 mt-1">
+          <div className="p-1 shadow-inner  rounded-full">
+            <FaLinkedin className="text-2xl" />
           </div>
 
-          <div>
-            <a href='!#'>Linkedin</a>
+          <div className="ml-3">
+            <a href="!#">Linkedin</a>
           </div>
         </div>
-        <div className='flex items-center gap-2 mt-1'>
-          <div className='p-1 shadow-inner bg-gray-400/[.15] rounded-full'>
-            <FaGlobe className='text-lg' />
+        <div className="flex items-center gap-4 mt-1">
+          <div className="p-1 shadow-inner  rounded-full">
+            <FaGlobe className="text-2xl" />
           </div>
-          <div>
-            <a href='!#'>Portfolio</a>
+          <div className="ml-3">
+            <a href="!#">Portfolio</a>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Navbar() {
-  return <div className="w-screen h-16 "></div>;
+  return <div className=" h-16 "></div>;
 }
