08c2772415c4451d649df4d73be328ed56cc9df7
Massii <massinechiche@gmail.com>
2022-02-16 09:39:03 +0100

    style: user card tweak

--- src/Layout/Home/Home.js
+++ src/Layout/Home/Home.js
@@ -1,18 +1,18 @@
-import React from "react";
-import Users from "./Users";
+import React from 'react'
+import Users from './Users'
 
 export default function Home() {
-  return (
-    <div className=" flex justify-center ">
-      <div className="max-w-6xl flex flex-wrap mt-8">
-        <Users />
-        <Users />
-        <Users />
-        <Users />
-        <Users />
-        <Users />
-        <Users />
-      </div>
-    </div>
-  );
+    return (
+        <div className=" flex justify-center ">
+            <div className="max-w-6xl flex flex-wrap mt-8 ">
+                <Users />
+                <Users />
+                <Users />
+                <Users />
+                <Users />
+                <Users />
+                <Users />
+            </div>
+        </div>
+    )
 }
--- src/Layout/Home/Users.js
+++ src/Layout/Home/Users.js
@@ -1,50 +1,47 @@
-import React from "react";
-import { FaGithub, FaLinkedin, FaGlobe } from "react-icons/fa";
+import React from 'react'
+import { FaGithub, FaLinkedin, FaGlobe } from 'react-icons/fa'
 
 export default function Users() {
-  return (
-    <div
-      className="w-48 h-fit ml-8 mt-8  border-gray-900 px-8 py-6"
-      style={{ border: "1px solid #f0f0f0 " }}
-    >
-      <div className="w-full flex justify-center items-center">
-        <div>
-          <img
-            src="https://api.uifaces.co/our-content/donated/IPh6PTBx.jpg"
-            alt=""
-            className="rounded-full p-4"
-          />
-          <div className="text-center ">John Doe</div>
-        </div>
-      </div>
-      <div className="w-full text-gray-800 mt-2">
-        <div className="flex items-center gap-4">
-          <div className="p-1 shadow-inner  rounded-full">
-            <FaGithub className=" text-2xl" />
-          </div>
+    return (
+        <div className="w-48 h-fit ml-8 mt-8 border  border-gray-200 px-8 py-4">
+            <div className="w-full flex justify-center items-center">
+                <div>
+                    <img
+                        src="https://api.uifaces.co/our-content/donated/IPh6PTBx.jpg"
+                        alt=""
+                        className="rounded-full p-2"
+                    />
+                    <div className="text-center font-semibold">John Doe</div>
+                </div>
+            </div>
+            <div className="w-full text-gray-500 mt-6">
+                <div className="flex items-center gap-4">
+                    <div className="p-1 shadow-inner  rounded-full">
+                        <FaGithub className=" text-2xl" />
+                    </div>
 
-          <div className="ml-3">
-            <a href="!#">Github</a>
-          </div>
-        </div>
-        <div className="flex items-center gap-4 mt-1">
-          <div className="p-1 shadow-inner  rounded-full">
-            <FaLinkedin className="text-2xl" />
-          </div>
+                    <div className="ml-3 text-gray-800 text-sm">
+                        <a href="!#">Github</a>
+                    </div>
+                </div>
+                <div className="flex items-center gap-4 mt-1">
+                    <div className="p-1 shadow-inner  rounded-full">
+                        <FaLinkedin className="text-2xl" />
+                    </div>
 
-          <div className="ml-3">
-            <a href="!#">Linkedin</a>
-          </div>
+                    <div className="ml-3 text-gray-800 text-sm ">
+                        <a href="!#">Linkedin</a>
+                    </div>
+                </div>
+                <div className="flex items-center gap-4 mt-1">
+                    <div className="p-1 shadow-inner  rounded-full">
+                        <FaGlobe className="text-2xl" />
+                    </div>
+                    <div className="ml-3 text-gray-800 text-sm">
+                        <a href="!#">Portfolio</a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="flex items-center gap-4 mt-1">
-          <div className="p-1 shadow-inner  rounded-full">
-            <FaGlobe className="text-2xl" />
-          </div>
-          <div className="ml-3">
-            <a href="!#">Portfolio</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    )
 }
